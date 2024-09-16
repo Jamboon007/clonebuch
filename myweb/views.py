c7f2fd9bbbc941de9f6cf68038fcddc55f68b9d7
BuchPawares <125111002+BuchPawares@users.noreply.github.com>
2024-09-16 21:01:04 +0700

    sum

--- myweb/views.py
+++ myweb/views.py
@@ -6,9 +6,8 @@ def index(request):
     return render(request, 'index.html')
 
 def story(request):
-    #response=requests.get('https://buchpawares.github.io/my_measure/measure.json').json()
-    #{'response':response}
-    return render(request, 'story.html')
+    response=requests.get('https://buchpawares.github.io/my_measure/measure.json').json()
+    return render(request, 'story.html',{'response':response})
 
 def contact(request):
     return render(request, 'contact.html')

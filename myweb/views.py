4bbe44a4944a81a3cece9915be7d021311784951
BuchPawares <125111002+BuchPawares@users.noreply.github.com>
2024-09-16 20:50:43 +0700

    cc

--- myweb/views.py
+++ myweb/views.py
@@ -3,13 +3,11 @@ import requests
 
 # Create your views here.
 def index(request):
-    response=requests.get('https://buchpawares.github.io/my_measure/measure.json').json()
-    return render(request, 'index.html',{'response':response})
+    return render(request, 'index.html')
 
 def story(request):
     response=requests.get('https://buchpawares.github.io/my_measure/measure.json').json()
     return render(request, 'story.html',{'response':response})
 
 def contact(request):
-    response=requests.get('https://buchpawares.github.io/my_measure/measure.json').json()
-    return render(request, 'contact.html',{'response':response})
+    return render(request, 'contact.html')

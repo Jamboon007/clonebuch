878850929f5d436a814e771b921cdc423e3c3ec8
BuchPawares <125111002+BuchPawares@users.noreply.github.com>
2024-09-16 20:55:19 +0700

    Update views.py

--- myweb/views.py
+++ myweb/views.py
@@ -8,7 +8,7 @@ def index(request):
 def story(request):
     #response=requests.get('https://buchpawares.github.io/my_measure/measure.json').json()
     #{'response':response}
-    return render(request, 'story.html',)
+    return render(request, 'story.html')
 
 def contact(request):
     return render(request, 'contact.html')

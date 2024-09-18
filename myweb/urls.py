47049913a86bcb9f85d2c9b23fc15682f84f7a66
BuchPawares <125111002+BuchPawares@users.noreply.github.com>
2024-09-18 13:35:16 +0700

    Update urls.py

--- myweb/urls.py
+++ myweb/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("about", views.story, name="story"),
+    path("about", views.index, name="index"),
+    path("", views.story, name="story"),
     path("contact", views.contact, name="contact"),
 
 ]
